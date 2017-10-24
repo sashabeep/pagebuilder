b733354357a7bbda6683c95b02aa63a993ae108c
Sasha Beep <thecomicz@gmail.com>
2017-10-24 20:49:46 +0300

    Update setup.data.sql

--- install/setup.data.sql
+++ install/setup.data.sql
@@ -1,10 +1,13 @@
 CREATE TABLE IF NOT EXISTS `{PREFIX}pagebuilder` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `document_id` int(10) unsigned NOT NULL,
+  `container` varchar(255) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `config` varchar(255) NOT NULL,
   `values` mediumtext NOT NULL,
+  `visible` tinyint(1) unsigned DEFAULT 1
   `index` smallint(5) unsigned NOT NULL,
+  `container` varchar(255) DEFAULT NULL
   PRIMARY KEY (`id`),
   KEY `document_id` (`document_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -14,11 +17,6 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}pagebuilder` (
 
 # adding column for sections container
 
-ALTER TABLE {PREFIX}pagebuilder ADD COLUMN container varchar(255) DEFAULT NULL AFTER document_id;
+# ALTER TABLE {PREFIX}pagebuilder ADD COLUMN container varchar(255) DEFAULT NULL AFTER document_id;
 ALTER TABLE {PREFIX}pagebuilder DROP INDEX `document_id`;
 ALTER TABLE {PREFIX}pagebuilder ADD INDEX `document_id` (`document_id`, `container`);
-
-# Adding visibility option
-
-ALTER TABLE {PREFIX}pagebuilder ADD COLUMN visible tinyint(1) unsigned DEFAULT 1 AFTER `values`;
-
